ff627dce91a7657f3408b318200df8a7409d54f9
孙海燕 <1572296165@qq.com>
2018-11-30 18:13:36 +0800

    完善了dev-roles

--- src/components/roles/roles.vue
+++ src/components/roles/roles.vue
@@ -12,13 +12,34 @@
     <el-table :data="rolesList" style="width: 100%">
       <el-table-column type="expand" label="#" width="80">
         <template slot-scope="scope">
+          <!-- 一级权限 -->
           <el-row class="level1" v-for="item1 in scope.row.children" :key="item1.id">
             <el-col :span="4">
-              <el-tag >
+              <el-tag closable @close="handelClose(scope.row,item1.id)">
                 {{item1.authName}}
               </el-tag>
             </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="item2 in item1.children" :key="item2.id">
+                <!-- 二级权限 -->
+                <el-col :span="4">
+                  <el-tag closable type="success" @close="handelClose(scope.row,item2.id)">
+                    {{item2.authName}}
+                  </el-tag>
+                </el-col>
+                <!-- 三级权限 -->
+                <el-col :span="20">
+                  <el-tag closable @close="handelClose(scope.row,item3.id)" type="warning" class="level3" v-for="item3 in item2.children" :key="item3.id">
+                    {{item3.authName}}
+                  </el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
           </el-row>
+          <!-- 权限为空时 -->
+          <span v-if="scope.row.children.length===0">
+              未分配权限
+            </span>
         </template>
       </el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="180">
@@ -29,10 +50,23 @@
         <template slot-scope="scope">
           <el-button @click="editFormData(scope.row)" type="primary" plain size="mini" icon="el-icon-edit" circle></el-button>
           <el-button @click="deleUser(scope.row.id)" type="danger" plain size="mini" icon="el-icon-delete" circle></el-button>
-          <el-button @click="roleCheck(scope.row)" type="success" plain size="mini" icon="el-icon-check" circle></el-button>
+          <el-button @click="rightsPlay(scope.row)" type="success" plain size="mini" icon="el-icon-check" circle></el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 弹框 -->
+    <el-dialog title="收货地址" :visible.sync="rightsdialogFormVisible">
+      <!-- 
+        :default-expanded-keys="[2, 3]" 
+        :default-checked-keys="[5]"
+       -->
+      <el-tree :data="rightsTree" ref=tree :default-expanded-keys="expendList" :default-checked-keys="checkedList" show-checkbox node-key="id" :props="defaultProps" default-expand-all>
+      </el-tree>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="rightsdialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="getRolesRights()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -40,13 +74,101 @@
 export default {
   data() {
     return {
-      rolesList: []
+      rolesList: [],
+      rightsdialogFormVisible: false,
+      rightsTree: {},
+      defaultProps: {
+        children: "children",
+        label: "authName"
+      },
+      currentRolesId: -1,
+      checkedList: [],
+      expendList:[]
     }
   },
   created() {
     this.loadRoles()
   },
   methods: {
+    //角色授权
+    async getRolesRights(){
+      // console.log(this.currentRolesId)
+      let getCheckedAll = this.$refs.tree.getCheckedKeys()
+      let getHalfCheckedAll = this.$refs.tree.getHalfCheckedKeys()
+      let keysAll=[...getCheckedAll,...getHalfCheckedAll]
+      const res = await this.axios.post(`roles/${this.currentRolesId}/rights`,{
+        rids:keysAll.join(',')
+      })
+      console.log(res)
+      const{data,meta:{msg,status}}=res.data
+      if(status===200){
+        this.$message.success(msg)
+        this.rightsdialogFormVisible=false
+        this.loadRoles()
+      }else{
+        this.$message.error(msg)
+      }
+    },
+    //角色权限展示
+    async rightsPlay(rights) {
+      this.currentRolesId=rights.id
+      const res = await this.axios.get("rights/tree")
+      // console.log(res)
+      const {
+        data,
+        meta: {
+          msg,
+          status
+        }
+      } = res.data
+      if (status === 200) {
+        this.rightsTree = data
+        this.$message.success(msg)
+        this.currentRolesId = rights.id
+        const arr = []
+        this.rightsTree.forEach((item1) => {
+          item1.children.forEach((item2) => {
+            item2.children.forEach((item3) => {
+              arr.push(item3.id)
+            })
+          })
+        })
+        this.expendList = arr
+
+        //获取角色权限
+        const arr1 = []
+        rights.children.forEach((item1)=>{
+          item1.children.forEach((item2)=>{
+            item2.children.forEach((item3)=>{
+              arr1.push(item3.id)
+            })
+          })
+        })
+        this.checkedList=arr1
+        this.rightsdialogFormVisible = true
+      } else {
+        this.$message.error(msg)
+      }
+    },
+    //权限管理关闭
+    async handelClose(roles, rightsId) {
+      const res = await this.axios.delete(`roles/${roles.id}/rights/${rightsId}`)
+      console.log(res)
+      const {
+        meta: {
+          msg,
+          status
+        },
+        data
+      } = res.data
+      if (status === 200) {
+        this.$message.success(msg)
+        roles.children = data
+      } else {
+        this.$message.error(msg)
+      }
+    },
+    // 查询角色
     async loadRoles() {
       var AUTH_TOKEN = localStorage.getItem('token')
       this.axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,10 @@ import Home from '@/components/home/home.vue'
 import Users from '@/components/user/users.vue'
 import Rights from '@/components/roles/rights.vue'
 import Roles from '@/components/roles/roles.vue'
+import Goods from '@/components/goods/goods.vue'
 Vue.use(Router)
-
-export default new Router({
+const {Message}=()=>'element-ui'
+const router = new Router({
   routes: [
     {
       name:'login',
@@ -29,7 +30,27 @@ export default new Router({
         name:'roles',
         path:'/roles',
         component:Roles
+      },{
+        name:'goods',
+        path:'/goods',
+        component:Goods
       }]
     }
   ]
 })
+//路由的前置守卫
+router.beforeEach((to,from,next)=>{
+  if(to.name==="login"){
+    next()
+  } else {
+    const token = localStorage.getItem('token')
+    if(!token){
+      router.push({name:'login'})
+      Message.warning('请先登录')
+      return
+    }else {
+      next()
+    }
+  }
+})
+export default router
